28966ce353eef0a3aba961628c4e6a9a112fa291
Nick Galvez <nicholas.galvez@gmail.com>
2024-04-22 18:54:54 -0700

    Add temp sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 #include "wifi_save.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature sensors(&oneWire);
 
 void setup()
 {
@@ -11,10 +22,19 @@ void setup()
     {
         Serial.println("Connect WIFI FAULT");
     }
+    sensors.begin();
 }
 
 void loop()
 {
+    sensors.requestTemperatures();
+    float temperatureC = sensors.getTempCByIndex(0);
+    float temperatureF = sensors.getTempFByIndex(0);
+    Serial.print(temperatureC);
+    Serial.println("ºC");
+    Serial.print(temperatureF);
+    Serial.println("ºF");
+    delay(5000);
+
     Serial.println("loop");
-    delay(3000);
 }
